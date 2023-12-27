60a831a9c125c1f1d727135b3822daf55143e5f5
Victor <VictorCarmona9035@hotmail.com>
2023-12-27 21:12:51 +0100

    Routing y paso de parametros con Flask

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 '''http://localhost:5000/'''
 @app.route('/')
 def hello_world():
+    app.logger.info(f'Hemos entrado al path {request.path}')
     return 'Hola Mundo desde Flask'
+
+@app.route('/saludar/<nombre>')
+def saludar(nombre):
+    app.logger.info(f'Hemos entrado al path {request.path}')
+    return f'Saludos {nombre.upper()}'
+
+
+@app.route('/edad/<int:edad>')
+def edad(edad):
+    app.logger.info(f'Hemos entrado al path {request.path}')
+    return f'Tu edad es: {edad}'
